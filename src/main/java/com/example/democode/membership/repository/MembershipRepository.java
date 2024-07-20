eb2fa4a946ad15b8f9db728014aeda9b3feacee8
Aries <dolshei.work@gmail.com>
2024-07-20 16:01:37 +0900

    Membership 이 존재하는지 테스트 추가

--- src/main/java/com/example/democode/membership/repository/MembershipRepository.java
+++ src/main/java/com/example/democode/membership/repository/MembershipRepository.java
@@ -1,7 +1,9 @@
 package com.example.democode.membership.repository;
 
 import com.example.democode.membership.entity.Membership;
+import com.example.democode.membership.entity.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
+    Membership findByUserIdAndMembershipType(String userId, MembershipType membershipType);
 }
--- src/test/java/com/example/democode/membership/repository/MembershipRepositoryTest.java
+++ src/test/java/com/example/democode/membership/repository/MembershipRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.example.democode.membership.repository;
 
 import com.example.democode.membership.entity.Membership;
+import com.example.democode.membership.entity.MembershipType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +22,11 @@ public class MembershipRepositoryTest {
 
     @DisplayName("멤버쉽 등록")
     @Test
-    public void MembershipRegist() {
+    public void MembershipRegistTest() {
         // given (준비) : 어떠한 데이터가 준비되었을 때
         final Membership membership = Membership.builder()
                 .userId("userId")
-                .membershipName("네이버")
+                .membershipType(MembershipType.NAVER)
                 .point(10000)
                 .build();
 
@@ -35,7 +36,29 @@ public class MembershipRepositoryTest {
         // then (검증) : 어떠한 결과가 나와야 한다.
         assertThat(savedMembership.getId()).isNotNull();
         assertThat(savedMembership.getUserId()).isEqualTo("userId");
-        assertThat(savedMembership.getMembershipName()).isEqualTo("네이버");
+        assertThat(savedMembership.getMembershipType()).isEqualTo(MembershipType.NAVER);
         assertThat(savedMembership.getPoint()).isEqualTo(10000);
+    }
+
+    @DisplayName("멤버쉽이 존재하는지 테스트")
+    @Test
+    public void MembershipExistTest() {
+        // given (준비) : 어떠한 데이터가 준비되었을 때
+        final Membership membership = Membership.builder()
+                .userId("userId")
+                .membershipType(MembershipType.NAVER)
+                .point(10000)
+                .build();
+
+        // when (실행) : 어떠한 함수를 실행하면
+        membershipRepository.save(membership);
+        final Membership findMembership = membershipRepository.findByUserIdAndMembershipType("userId", MembershipType.NAVER);
+
+        // then (검증) : 어떠한 결과가 나와야 한다.
+        assertThat(findMembership).isNotNull();
+        assertThat(findMembership.getId()).isNotNull();
+        assertThat(findMembership.getUserId()).isEqualTo("userId");
+        assertThat(findMembership.getMembershipType()).isEqualTo(MembershipType.NAVER);
+        assertThat(findMembership.getPoint()).isEqualTo(10000);
     }
 }
