9c12dc98fd598843e83b137b03466b839fcaffb0
MacBook <leechanghun@outlook.com>
2024-07-27 10:39:52 +0900

    오타 수정

--- src/main/java/com/example/democode/global/config/WebClientConfig.java
+++ src/main/java/com/example/democode/global/config/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.example.democode.global.conig;
+package com.example.democode.global.config;
 
 import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
