782702077c00fe187fec369559afe622f9fbd49f
Aries <dolshei.work@gmail.com>
2024-08-04 03:16:41 +0900

    멤버쉽 조회 테스트코드 추가

--- src/main/java/com/example/democode/domain/membership/repository/MembershipRepository.java
+++ src/main/java/com/example/democode/domain/membership/repository/MembershipRepository.java
@@ -4,6 +4,10 @@ import com.example.democode.domain.membership.entity.Membership;
 import com.example.democode.domain.membership.model.MembershipType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MembershipRepository extends JpaRepository<Membership, Long> {
     Membership findByUserIdAndMembershipType(String userId, MembershipType membershipType);
+
+    List<Membership> findAllByUserId(String userId);
 }
