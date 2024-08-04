8daa7b95405fcf40e540a528cafb8f5e6b26bcfd
Aries <dolshei.work@gmail.com>
2024-08-04 15:47:34 +0900

    DTO 리팩토링

---
+++ src/main/java/com/example/democode/domain/membership/dto/MembershipAddResponse.java
@@ -0,0 +1,15 @@
+package com.example.democode.domain.membership.dto;
+
+import com.example.democode.domain.membership.model.MembershipType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class MembershipAddResponse {
+
+    private final Long id;
+    private final MembershipType membershipType;
+}
