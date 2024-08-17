82376b2513a3871e6bb6e23a43ed62980ce11684
MacBook <leechanghun@outlook.com>
2024-08-18 08:09:07 +0900

    Membership 상세정보 가져오기 추가

--- src/main/java/com/example/democode/domain/membership/service/MembershipService.java
+++ src/main/java/com/example/democode/domain/membership/service/MembershipService.java
@@ -7,19 +7,22 @@ import com.example.democode.domain.membership.model.MembershipErrorResult;
 import com.example.democode.domain.membership.model.MembershipType;
 import com.example.democode.domain.membership.exception.MembershipException;
 import com.example.democode.domain.membership.repository.MembershipRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class MembershipService {
 
     private final MembershipRepository membershipRepository;
 
-    public MembershipService(MembershipRepository membershipRepository) {
-        this.membershipRepository = membershipRepository;
-    }
+//    public MembershipService(MembershipRepository membershipRepository) {
+//        this.membershipRepository = membershipRepository;
+//    }
 
     public MembershipAddResponse addMembership(final String userId, final MembershipType membershipType, final Integer point) {
         final Membership result = membershipRepository.findByUserIdAndMembershipType(userId, membershipType);
@@ -41,17 +44,33 @@ public class MembershipService {
                 .build();
     }
 
-    public List<MembershipDetailResponse> getMembershipList(String userId) {
+    public List<Membership> getMembershipList(String userId) {
 
-        final List<MembershipDetailResponse> membershipList = membershipRepository.findAllByUserId(userId);
+        final List<Membership> membershipList = membershipRepository.findAllByUserId(userId);
         return membershipList.stream()
-                .map(v -> MembershipDetailResponse.builder()
+                .map(v -> Membership.builder()
                         .id(v.getId())
                         .membershipType(v.getMembershipType())
                         .point(v.getPoint())
                         .createdAt(v.getCreatedAt())
                         .build()
                 ).collect(Collectors.toList());
+    }
+
+    public MembershipDetailResponse getMembership(final Long membershipId, final String userId) {
+        final Optional<Membership> optionalMembership = membershipRepository.findById(membershipId);
+        final Membership membership = optionalMembership.orElseThrow(() -> new MembershipException(MembershipErrorResult.MEMBERSHIP_NOT_FOUND));
+
+        if (!membership.getUserId().equals(userId)) {
+            throw new MembershipException(MembershipErrorResult.NOT_MEMBERSHIP_OWNER);
+        }
+
+        return MembershipDetailResponse.builder()
+                .id(membership.getId())
+                .membershipType(membership.getMembershipType())
+                .point(membership.getPoint())
+                .createdAt(membership.getCreatedAt())
+                .build();
     }
 
 }
